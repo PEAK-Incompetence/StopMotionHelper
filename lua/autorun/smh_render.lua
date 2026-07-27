a365edd4f0001909d7f4f3719d79471399ee6d48
vlazed <35244845+vlazed@users.noreply.github.com>
2026-07-27 19:23:41 -0400

    Make render bounds also scale with other parameters - Scale with model scale or prop resizer scale

--- lua/autorun/smh_render.lua
+++ lua/autorun/smh_render.lua
@@ -1,19 +1,32 @@
 -- Prevent props from disappearing when resized.
+---@class Entity
+local ENTITY = FindMetaTable("Entity")
 
 if SERVER then
     local forceRenderBoundsCVar = CreateConVar("smh_force_render_bounds", "1", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "If set to 1, it sets the render bounds of entity when using scaling an entity with bone manipulations. This is useful for ensuring resized props do not disappear", 0, 1)
-    local forceRenderBounds = forceRenderBoundsCVar:GetBool()
-    cvars.AddChangeCallback(forceRenderBoundsCVar:GetName(), function (convar, oldValue, newValue)
-        forceRenderBounds = tobool(newValue)
-    end)
-    
-    util.AddNetworkString("SMHForceRenderBounds")
-    ---@class Entity
-    local ENTITY = FindMetaTable("Entity")
+        local forceRenderBounds = forceRenderBoundsCVar:GetBool()
+        cvars.AddChangeCallback(forceRenderBoundsCVar:GetName(), function (convar, oldValue, newValue)
+            forceRenderBounds = tobool(newValue)
+        end)
+        
+        util.AddNetworkString("SMHForceRenderBoundsModelScale")
+        util.AddNetworkString("SMHForceRenderBoundsBoneScale")
+
+    ENTITY.smh_SetModelScale = ENTITY.smh_SetModelScale or ENTITY.SetModelScale
+    function ENTITY:SetModelScale(scale, deltaTIme, ...)
+        if forceRenderBounds then
+            net.Start("SMHForceRenderBoundsModelScale")
+            net.WriteEntity(self)
+            net.WriteDouble(scale)
+            net.Broadcast()
+        end
+        return self:smh_SetModelScale(scale, deltaTime, ...)
+    end
+
     ENTITY.smh_ManipulateBoneScale = ENTITY.smh_ManipulateBoneScale or ENTITY.ManipulateBoneScale
     function ENTITY:ManipulateBoneScale(i, scale, ...)
         if forceRenderBounds then
-            net.Start("SMHForceRenderBounds")
+            net.Start("SMHForceRenderBoundsBoneScale")
             net.WriteEntity(self)
             net.WriteUInt(i, 8)
             net.WriteVector(scale)
@@ -28,23 +41,44 @@ end
 ---@param boneID number
 ---@param scale Vector|number
 local function setScaledRenderBounds(ent, boneID, scale)
-    local oldMin, oldMax = ent.smh_RenderBoundsCacheMin, ent.smh_RenderBoundsCacheMax
+    local oldMin, oldMax = ent.smh_RenderBoundsCacheMinOG, ent.smh_RenderBoundsCacheMaxOG
 
-    local min = oldMin * scale
+    scale = scale * ent.smh_RenderBoundsCacheMatrixScale * ent.smh_RenderBoundsCacheModelScale
+    local min = oldMin * scale 
     local max = oldMax * scale
     
     ent:SetRenderBounds(min, max)
 end
 
-hook.Add("OnEntityCreated", "SMHForceRenderBoundsInitialize", function (entity)
-    if IsValid(entity) then
-        entity.smh_RenderBoundsCacheMin, entity.smh_RenderBoundsCacheMax = entity:GetRenderBounds()
+---@param entity Entity
+local function initializeRenderBounds(entity)
+    if IsValid(entity) and not entity.smh_RenderBoundsCacheMinOG then
+        entity.smh_RenderBoundsCacheMinOG, entity.smh_RenderBoundsCacheMaxOG = entity:GetRenderBounds()
+        entity.smh_RenderBoundsCacheModelScale = entity:GetModelScale()
+        entity.smh_RenderBoundsCacheMatrixScale = Vector(1, 1, 1)
     end
-end)
+end
 
-net.Receive("SMHForceRenderBounds", function (len, ply)
+
+hook.Add("OnEntityCreated", "SMHForceRenderBoundsInitialize", initializeRenderBounds)
+
+net.Receive("SMHForceRenderBoundsBoneScale", function (len, ply)
     local ent = net.ReadEntity()
     local index = net.ReadUInt(8)
     local scale = net.ReadVector()
+    initializeRenderBounds(entity)
     setScaledRenderBounds(ent, index, scale)
 end)
+
+net.Receive("SMHForceRenderBoundsModelScale", function (len, ply)
+    local entity = net.ReadEntity()
+    local scale = net.ReadDouble()
+    initializeRenderBounds(entity)
+    entity.smh_RenderBoundsCacheModelScale = scale
+end)
+
+ENTITY.smh_EnableMatrix = ENTITY.smh_EnableMatrix or ENTITY.EnableMatrix
+function ENTITY:EnableMatrix(matrixType, matrix, ...)
+    self.smh_RenderBoundsCacheMatrixScale = matrix:GetScale()
+    return self:smh_EnableMatrix(matrixType, matrix, ...)
+end
