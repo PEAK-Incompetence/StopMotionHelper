92d740d088dafad01f0199d6ec35c3d3888e4bd2
vlazed <35244845+vlazed@users.noreply.github.com>
2026-07-29 20:57:54 -0400

    Fix errors when rendering ghosts - Fix error related to trying to set render bounds for ghost entities (they don't spawn at the same frame in multiplayer) - Fix render bounds not changing for ghost entities. There is a delay before this happens. Fortunately, such delay only happens in multiplayer (or in some special cases. Please let me know the addon that might be causing this!)

--- lua/autorun/smh_render.lua
+++ lua/autorun/smh_render.lua
@@ -1,4 +1,7 @@
 -- Prevent props from disappearing when resized.
+
+---@type fun(index: integer): SMHEntity
+local Entity = Entity
 ---@class Entity
 local ENTITY = FindMetaTable("Entity")
 
@@ -37,48 +40,136 @@ if SERVER then
     return 
 end
 
----@param ent Entity
----@param boneID number
----@param scale Vector|number
-local function setScaledRenderBounds(ent, boneID, scale)
-    local oldMin, oldMax = ent.smh_RenderBoundsCacheMinOG, ent.smh_RenderBoundsCacheMaxOG
-
-    scale = scale * ent.smh_RenderBoundsCacheMatrixScale * ent.smh_RenderBoundsCacheModelScale
-    local min = oldMin * scale 
-    local max = oldMax * scale
-    
-    ent:SetRenderBounds(min, max)
-end
-
----@param entity Entity
-local function initializeRenderBounds(entity)
-    if IsValid(entity) and not entity.smh_RenderBoundsCacheMinOG then
-        entity.smh_RenderBoundsCacheMinOG, entity.smh_RenderBoundsCacheMaxOG = entity:GetRenderBounds()
-        entity.smh_RenderBoundsCacheModelScale = entity:GetModelScale()
-        entity.smh_RenderBoundsCacheMatrixScale = Vector(1, 1, 1)
-    end
-end
-
-
-hook.Add("OnEntityCreated", "SMHForceRenderBoundsInitialize", initializeRenderBounds)
-
-net.Receive("SMHForceRenderBoundsBoneScale", function (len, ply)
-    local ent = net.ReadEntity()
-    local index = net.ReadUInt(8)
-    local scale = net.ReadVector()
-    initializeRenderBounds(entity)
-    setScaledRenderBounds(ent, index, scale)
-end)
-
-net.Receive("SMHForceRenderBoundsModelScale", function (len, ply)
-    local entity = net.ReadEntity()
-    local scale = net.ReadDouble()
-    initializeRenderBounds(entity)
-    entity.smh_RenderBoundsCacheModelScale = scale
-end)
-
 ENTITY.smh_EnableMatrix = ENTITY.smh_EnableMatrix or ENTITY.EnableMatrix
 function ENTITY:EnableMatrix(matrixType, matrix, ...)
     self.smh_RenderBoundsCacheMatrixScale = matrix:GetScale()
     return self:smh_EnableMatrix(matrixType, matrix, ...)
 end
+
+---@param ent SMHEntity
+---@param boneID number
+---@param scale Vector|number
+local function setScaledRenderBounds(ent, boneID, scale)
+    if IsValid(ent) then
+        local oldMin, oldMax = ent.smh_RenderBoundsCacheMinOG, ent.smh_RenderBoundsCacheMaxOG
+
+        scale = scale * ent.smh_RenderBoundsCacheMatrixScale * ent.smh_RenderBoundsCacheModelScale
+        local min = oldMin * scale 
+        local max = oldMax * scale
+        
+        ent:SetRenderBounds(min, max)
+    end
+end
+
+---@type Set<string>
+local invalidEntities = {
+    ["class CLuaEffect"] = true
+}
+
+---@type Set<{[1]: Vector, [2]: Vector}>
+local modelRenderBounds = {}
+
+---@param entity SMHEntity
+---@return Vector
+---@return Vector
+---@return function
+local function cacheRenderBounds(entity)
+    local model = entity:GetModel()
+    local renderBounds = modelRenderBounds[model]
+
+    local function remove()
+    end
+    if not renderBounds and model then
+        -- We need to spawn a clientside model with the default render bounds
+        local csModel = ents.CreateClientProp()
+        csModel:SetModel(model)
+        csModel:SetupBones()
+        csModel:InvalidateBoneCache()
+        csModel:Spawn()
+
+        local min, max = csModel:GetRenderBounds()
+        renderBounds = {min, max}
+        modelRenderBounds[model] = renderBounds
+        remove = function()
+            csModel:Remove()
+        end
+    end
+    
+    return renderBounds[1], renderBounds[2], remove
+end
+
+local busy = false
+---@param entity SMHEntity
+local function initializeRenderBounds(entity)
+    if busy then return end
+
+    busy = true
+    local success, result = pcall(function()
+        if 
+            IsValid(entity) 
+            and not invalidEntities[entity:GetClass()] 
+            and entity.GetModel 
+            and entity:GetModel() 
+            and not entity:IsWeapon() 
+            and not entity.smh_RenderBoundsCacheMinOG
+        then
+            local min, max, remove = cacheRenderBounds(entity)
+            entity.smh_RenderBoundsCacheMinOG, entity.smh_RenderBoundsCacheMaxOG = min, max
+            remove()
+
+            entity.smh_RenderBoundsCacheModelScale = entity:GetModelScale()
+            entity.smh_RenderBoundsCacheMatrixScale = Vector(1, 1, 1)
+        end
+    end)
+    if not success and result then
+        ErrorNoHaltWithStack(result .. "\n")
+    end
+    busy = false
+end
+
+---@param ent SMHEntity
+---@param index integer
+---@param scale Vector
+local function setEntityRenderBounds(ent, index, scale)
+    initializeRenderBounds(ent)
+    setScaledRenderBounds(ent, index, scale)
+end
+
+net.Receive("SMHForceRenderBoundsBoneScale", function (len, ply)
+    local entIndex = net.ReadUInt(MAX_EDICT_BITS)
+    local index = net.ReadUInt(8)
+    local scale = net.ReadVector()
+    local entity = Entity(entIndex)
+    if IsValid(entity) then
+        setEntityRenderBounds(entity, index, scale)
+    else
+        -- In multiplayer (or other cases), the ghost entity isn't immediately available, so we have to wait an extra tick
+        -- and then try again. We only need to do this for ghost entities fortunately, but this is really hacky
+        timer.Simple(0.1, function()
+            entity = Entity(entIndex)
+            if IsValid(entity) then
+                setEntityRenderBounds(entity, index, scale)
+            end
+        end)
+    end
+end)
+
+net.Receive("SMHForceRenderBoundsModelScale", function (len, ply)
+    local entIndex = net.ReadUInt(MAX_EDICT_BITS)
+    local scale = net.ReadDouble()
+    local entity = Entity(entIndex)
+    if IsValid(entity) then
+        initializeRenderBounds(entity)
+        entity.smh_RenderBoundsCacheModelScale = scale
+    else
+        timer.Simple(0.1, function ()
+            entity = Entity(entIndex)
+            if IsValid(entity) then
+                initializeRenderBounds(entity)
+                entity.smh_RenderBoundsCacheModelScale = scale           
+            end
+        end)
+    end
+end)
+
+hook.Add("OnEntityCreated", "SMHForceRenderBoundsInitialize", initializeRenderBounds)
